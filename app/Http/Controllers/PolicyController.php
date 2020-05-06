20a8b5e56ec22a8f2cd45b53eb90106757f543e0
Radoslaw Bura <radoslaw.bura@gmail.com>
2020-05-06 20:54:06 +0200

    Updated PolicyController.php

--- app/Http/Controllers/PolicyController.php
+++ app/Http/Controllers/PolicyController.php
@@ -8,10 +8,21 @@ use Illuminate\Support\Facades\Session;
 
 class PolicyController extends Controller
 {
+
+  // ===============================================================
   public function __construct()
   {
     $this->middleware('auth');
+
+    // ------------------------------
+    // create file policy.txt if it doesn't exist
+    if (!Storage::disk('local')->exists('policy.txt')) {
+      // $path = Storage::disk('local');
+      Storage::disk('local')->put('policy.txt', 'no policy');
+    }
   }
+
+  // ===============================================================
   /**
    * Display a listing of the resource.
    *
@@ -19,23 +30,19 @@ class PolicyController extends Controller
    */
   public function index()
   {
-    if (Storage::exists('policy.txt')) {
-      $policy = Storage::get('policy.txt');
+    if (Storage::disk('local')->exists('policy.txt')) {
+      // ------------------------------
+      // get file
+      $policy = Storage::disk('local')->get('policy.txt');
+    } else if (!Storage::disk('local')->exists('policy.txt')) {
+      // $path = Storage::disk('local');
+      Storage::disk('local')->put('policy.txt', 'no policy');
     }
 
     return view('website-content.policy.index', ['policy' => $policy]);
   }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function create()
-  {
-    //
-  }
-
+  // ===============================================================
   /**
    * Store a newly created resource in storage.
    *
@@ -44,65 +51,23 @@ class PolicyController extends Controller
    */
   public function store(Request $request)
   {
+    // ------------------------------
+    // get new policy from textarea
     $policy = $request->policyContent;
 
-    if ($policy) {
-      if (Storage::exists('policy.txt')) {
-        Storage::put('policy.txt', $policy);
-      } else {
-        return 'error';
-      }
+    if ($policy && Storage::disk('local')->exists('policy.txt')) {
+      // ------------------------------
+      // update file
+      Storage::disk('local')->put('policy.txt', $policy);
+    } else if ($policy && !(Storage::disk('local')->exists('policy.txt'))) {
+      Storage::disk('local')->put('policy.txt', $policy);
     }
 
-    // Create new alerts
+    // ------------------------------
+    // alerts
     Session::flash('alert-message', 'Privacy & Cookies Policy updated successfully!');
     Session::flash('alert-class', 'alert-success');
 
     return redirect('dashboard/policy');
-  }
-
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function show($id)
-  {
-    //
-  }
-
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function edit($id)
-  {
-    //
-  }
-
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Request $request, $id)
-  {
-    //
-  }
-
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy($id)
-  {
-    //
   }
 }
